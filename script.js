856c1582ad10a805f08a9c59fd66b33984d2566b
Orr Kislev <orrkislev@gmail.com>
2020-05-01 16:09:15 +0300

    fix colors

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ colorPallets = [
     {"leftBG":"#4ecdc4",   "leftTxt":"#1a535c",   "rightBG":"#ff6b6b",   "rightTxt":"#ffe66d",   "btnBack":"#f7fff7"},
     {"leftBG":"#ffbf69",   "leftTxt":"#2ec4b6",   "rightBG":"#cbf3f0",   "rightTxt":"#ff9f1c",   "btnBack":"#ffffff"},
     {"leftBG":"#f2cc8f",   "leftTxt":"#e07a5f",   "rightBG":"#81b29a",   "rightTxt":"#3d405b",   "btnBack":"#f4f1de"},
-    {"leftBG":"#9ceaef",   "leftTxt":"#68d8d6",   "rightBG":"#3dccc7",   "rightTxt":"#68d8d6",   "btnBack":"#c4fff9"},
     {"leftBG":"#f86624",   "leftTxt":"#f9c80e",   "rightBG":"#662e9b",   "rightTxt":"#43bccd",   "btnBack":"#ea3546"},
-    {"leftBG":"#333533",   "leftTxt":"#f5cb5c",   "rightBG":"#cfdbd5",   "rightTxt":"#f5cb5c",   "btnBack":"#e8eddf"},
+    {"leftBG":"#333533",   "leftTxt":"#f5cb5c",   "rightBG":"#cfcfcf",   "rightTxt":"#333533",   "btnBack":"#e8eddf"},
 ]
 
 $("#txtRight").html(choose(list))
 $("#txtLeft").html(choose(list))
 
-pallet = choose(colorPallets)
+palletNum = Math.floor(Math.random()*colorPallets.length)
+console.log(palletNum)
+pallet = colorPallets[palletNum]
 $(".left").css("background-color",pallet["leftBG"])
 $(".right").css("background-color",pallet["rightBG"])
 $("#txtLeft").css("color",pallet["leftTxt"])
